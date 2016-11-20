868fccd8f31daf0f2fc5c300795dcf6cd56fad29
Brendan Conron <conronb@gmail.com>
2016-11-20 01:04:21 -0800

    added annotations and apperance providers

---
+++ Partybus/Mapbox/StopAnnotation.swift
@@ -0,0 +1,27 @@
+//
+//  StopAnnotation.swift
+//  Partybus
+//
+//  Created by Brendan Conron on 11/18/16.
+//  Copyright © 2016 Swoopy Studios. All rights reserved.
+//
+
+import UIKit
+import Mapbox
+
+class StopAnnotation: NSObject, MGLAnnotation {
+
+    var coordinate: CLLocationCoordinate2D
+    var title: String?
+    var subtitle: String?
+
+    var image: UIImage?
+    var reuseIdentifier: String?
+
+    init(coordinate: CLLocationCoordinate2D, title: String? = nil, subtitle: String? = nil) {
+        self.coordinate = coordinate
+        self.title = title
+        self.subtitle = subtitle
+    }
+
+}
